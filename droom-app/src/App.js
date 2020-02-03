09281d54ca465b5aa60257cbf169d041e4dcd20f
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-03 11:51:13 +0000

    setting up continue

--- droom-app/src/App.js
+++ droom-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
+      {/* //Set up Routes and components i want to render on initial display */}
       
+
+      <Home />
     </div>
   );
 }
---
+++ droom-app/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+export default function Home() {
+
+
+
+
+    return (
+
+        <div>
+            Hi from Home component
+        </div>
+    )
+
+}
