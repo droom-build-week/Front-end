7b433f3d799ad867de583fdfce0cd8fef4b63df1
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-03 12:24:04 +0000

    styling and content for first component, hom epage

--- droom-app/src/components/Home.js
+++ droom-app/src/components/Home.js
@@ -1,16 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
+
+//Styling the page
+
+const Div = styled.div `
+/* border: 1px red solid; */
+padding-top: 40px;
+margin-left: 20px;
+margin-right: 20px;
+
+
+`
+const Img = styled.img `
+object-fit: cover;
+width: 100%;
+height: 300px;
+
+
+`
+
+
+
+
 
 
 
 export default function Home() {
 
 
-
-
     return (
 
-        <div>
+        <div className="home-wrapper">
             Hi from Home component
+
+
+            <Div className="imagecontainer">
+            <Img className="home-image"
+				src="https://images.unsplash.com/photo-1526948128573-703ee1aeb6fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
+				alt="header"
+			/>
+
+            </Div>
+
+
+
+
+
+
+
+
+
         </div>
     )
 
