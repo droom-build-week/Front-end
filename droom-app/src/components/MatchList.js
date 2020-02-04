05b4e852d0a022aaf88e519f093f09a7b30e40ac
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-04 12:37:20 +0000

    style

--- droom-app/src/components/MatchList.js
+++ droom-app/src/components/MatchList.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
 //Component is the displays the list of matches as cards following swiping for the companies job listings they want
+
+
+const Div = styled.div `
+background: #4389A2;
+border: 3px solid lavender;
+
+`
+
 
 export default function MatchList () {
 
 
     return (
 
-        <div className="matchListWrapper">
+        <Div className="matchListWrapper">
            <h3>Check Out Your Job Matches </h3>
-        </div>
+        
+        
+        </Div>
     )
 }
